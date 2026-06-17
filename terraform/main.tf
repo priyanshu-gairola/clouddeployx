f0c0cafbbccbe8abf15c3d7ba586c9f994702d05
priyanshu-gairola <gairola.priyanshu03@gmail.com>
2026-06-17 21:50:53 +0530

    infra: provision security group using Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,24 @@
 resource "aws_ecr_repository" "clouddeployx_tf" {
   name = var.repository_name
 }
+
+
+resource "aws_security_group" "clouddeployx_sg" {
+  name        = "clouddeployx-sg"
+  description = "Security group for CloudDeployX"
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
